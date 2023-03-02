ef3fab5c5b54405e4cb8f77709e808c821bcaf29
viys <2628587386@qq.com>
2023-03-02 16:30:15 +0800

    add git.md

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,8 +1,7 @@
 module.exports = {
-  base: "./",
   //网站标题（博客名称）
   "title": "778B",
-  //加载页面的提示语句
+  //加载页面的提示语�?
   "description": "温柔才是宝藏，你也是",
   "dest": "dist",
   "head": [
@@ -44,7 +43,7 @@ module.exports = {
         "icon": "reco-home"
       },
       {
-        "text": "时间轴",
+        "text": "时间�?",
         "link": "/timeline/",
         "icon": "reco-date"
       },
@@ -70,13 +69,13 @@ module.exports = {
     "friendLink": [
       //http://43.138.16.164/
     ],
-    //配置评论，比较复杂建议自己百度
+    //配置评论，比较复杂建议自己百�?
     "valineConfig": {
       "appId": "XoYVGebrakOU2FA6fhWOCGxA-gzGzoHsz",// your appId
       "appKey": "z8fnOR0R5kyLwEAoTdszjTvE", // your appKey
       "avatar": "https://s2.loli.net/2022/06/14/fgbwH1c5dlZUiuy.jpg", //
       "enableQQ": true, //启用昵称框自动获取QQ昵称和QQ头像
-      "requiredFields": ['nick', 'mail'], //设置必填项
+      "requiredFields": ['nick', 'mail'], //设置必填�?
     },
     "logo": "/mylogo.png",
     // "huawei": true, //首页出现华为文案
@@ -85,16 +84,16 @@ module.exports = {
     "lastUpdated": "Last Updated",
     "author": "778B",
     "authorAvatar": "/avatar_top.png",
-    "record": "你的备案号 ", //icp备案
+    "record": "你的备案�? ", //icp备案
     "startYear": "2022",
-    //关于页面的信息
-    "info": "欢迎踏入IOT的世界",
+    //关于页面的信�?
+    "info": "欢迎踏入IOT的世�?",
     //社交账号设置
     "socials": {
       "github": "https://github.com/viys", //github
       //"gitlub": false, //gitlub
       //"gitee": false,//"https://gitee.com/GodLikeZeal", //gitee
-      //"jianshu":false,// "https://www.jianshu.com/u/e2d051b6d2e9", //简书
+      //"jianshu":false,// "https://www.jianshu.com/u/e2d051b6d2e9", //简�?
       //"zhihu": "https://www.zhihu.com/people/qian-ge-diao-63", //知乎
       //"toutiao": false, //知乎
       //"juejin": "https://juejin.cn/user/334694205359901", //掘金
@@ -105,38 +104,38 @@ module.exports = {
       "qq": "tencent://message/?Menu=yes&uin=2628587386&Service=300&sigT=45a1e5847943b64c6ff3990f8a9e644d2b31356cb0b4ac6b24663a3c8dd0f8aa12a595b1714f9d45" //QQ
     },
 
-    //首页文案，可以自己更换每天会自动显示一段
+    //首页文案，可以自己更换每天会自动显示一�?
     "mottos": [
     {
-      "zh": "天在下雨地下滑，自己跌倒自己爬。",
+      "zh": "天在下雨地下滑，自己跌倒自己爬�?",
       "en": "It was raining and I slipped. I fell and climbed."
     },
     {
-      "zh": "花有百般红，猫与猫不同。",
+      "zh": "花有百般红，猫与猫不同�?",
       "en": "Flowers are all kinds of red. People are different from dogs."
     },
     {
-      "zh": "万物与我都是荒诞的静寂",
+      "zh": "万物与我都是荒诞的静�?",
       "en": "Just because people gather and disperse in the wind, it's up to you and me."
     },
     {
-      "zh": "月亮很亮，亮也没用，没用也亮。",
+      "zh": "月亮很亮，亮也没用，没用也亮�?",
       "en": "After learning many skills, I finally found that the most useful skill is to be open-minded."
     },
     {
-      "zh": "现在，天色暗了，我也倦了。",
+      "zh": "现在，天色暗了，我也倦了�?",
       "en": "Enjoy yourself. These are the good old days you're going to miss in the years ahead. "
     }, {
       "zh": "孤独 养分 风与黄昏 水与等火 我都不缺",
       "en": "It's a bit careless to stand in the rain on a thundering day."
     },
     {
-      "zh": "你走的路要繁花盛开，要人声鼎沸。",
+      "zh": "你走的路要繁花盛开，要人声鼎沸�?",
       "en": "I just want to stand and earn money."
     }
     ],
-    //配置背景图片，每次刷新自动
-    //放public下名字跟下面一样
+    //配置背景图片，每次刷新自�?
+    //放public下名字跟下面一�?
     "covers": [
       '/homeImage/1.jpg',
       '/homeImage/2.jpg',
@@ -168,20 +167,20 @@ module.exports = {
   plugins:[
     ["ribbon-animation", {
       size: 90,   // 默认数据
-      opacity: 0.3,  //  透明度
+      opacity: 0.3,  //  透明�?
       zIndex: -1,   //  层级
       opt: {
-        // 色带HSL饱和度
+        // 色带HSL饱和�?
         colorSaturation: "80%",
-        // 色带HSL亮度量
+        // 色带HSL亮度�?
         colorBrightness: "60%",
-        // 带状颜色不透明度
+        // 带状颜色不透明�?
         colorAlpha: 0.65,
-        // 在HSL颜色空间中循环显示颜色的速度有多快
+        // 在HSL颜色空间中循环显示颜色的速度有多�?
         colorCycleSpeed: 6,
-        // 从哪一侧开始Y轴 (top|min, middle|center, bottom|max, random)
+        // 从哪一侧开始Y�? (top|min, middle|center, bottom|max, random)
         verticalPosition: "center",
-        // 到达屏幕另一侧的速度有多快
+        // 到达屏幕另一侧的速度有多�?
         horizontalSpeed: 200,
         // 在任何给定时间，屏幕上会保留多少条带
         ribbonCount: 2,
@@ -201,7 +200,7 @@ module.exports = {
   //   if (!isServer) {
   //     // 修改客户端的 webpack 配置
   //     config.output.publicPath = config.mode === 'production'
-  //       ? 'https://pan.zealsay.com/blog/' // sample/essays 打包的默认路径为 ‘_nuxt’ 或者可以指定cdn 域名
+  //       ? 'https://pan.zealsay.com/blog/' // sample/essays 打包的默认路径为 ‘_nuxt�? 或者可以指定cdn 域名
   //       : '/';
   //     config.output.filename = "assets/js/[name].js";
   //   }
